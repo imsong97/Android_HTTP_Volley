fa12bc8f1d95c4ed21f35fd53e371c09e9963e39
DyNamiC <imsong1997@gmail.com>
2020-12-08 12:15:49 +0900

    HTTP통신

---
+++ HTTPVolley/app/src/main/java/com/example/httpvolley/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.httpvolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    Button btn;
+    EditText userId, otp;
+    String get_addr = "http://192.168.0.9:9090/admin";
+    String post_addr = "http://192.168.0.9:9090/api/rest/otp/check";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView)findViewById(R.id.textView);
+        userId = (EditText)findViewById(R.id.userId);
+        otp = (EditText)findViewById(R.id.otp);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+
+        if(AppHelper.requestQueue != null) {
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+    }
+    public void sendRequest() {
+        StringRequest request = new StringRequest(Request.Method.GET, get_addr,
+                new Response.Listener<String>() { //응답을 잘 받았을 때 이 메소드가 자동으로 호출
+                    @Override
+                    public void onResponse(String response) {
+                        println("[응답]\n" + response);
+                        Toast.makeText(getApplicationContext(), "GET", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() { //에러 발생시 호출될 리스너 객체
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        println("GET Error -> " + error.getMessage());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String,String>();
+
+                return params;
+            }
+        };
+        request.setShouldCache(false); //이전 결과 있어도 새로 요청하여 응답을 보여준다.
+        AppHelper.requestQueue = Volley.newRequestQueue(this); // requestQueue 초기화 필수
+        AppHelper.requestQueue.add(request);
+        println("요청 보냄.");
+
+    }
+
+    public void println(String data) {
+        tv.setText(data +"\n");
+    }
+
+    public void onClick(View v){
+        JSONObject params = new JSONObject();
+        try {
+            params.put("userId", userId.getText().toString());
+            params.put("otp", otp.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, post_addr, params, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                tv.setText(response.toString());
+                Toast.makeText(getApplicationContext(), "POST", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                tv.setText("POST Error\n" + error);
+            }
+        });
+
+        stringRequest.setShouldCache(false);
+        AppHelper.requestQueue = Volley.newRequestQueue(this);
+        AppHelper.requestQueue.add(stringRequest);
+    }
+}
+
